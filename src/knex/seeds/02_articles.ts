e46dbcc1effadb309ace91c7be7268bfde4f3f64
yoni2356 <yoni2356@gmail.com>
2024-09-23 15:04:51 +0300

    include tsvector in seed

--- src/knex/seeds/02_articles.ts
+++ src/knex/seeds/02_articles.ts
@@ -11,12 +11,15 @@ export async function seed(knex: Knex): Promise<void> {
         {
             author_id: users[0].id,
             title: 'The gibberish',
-            body: 'hello Ipsum hello is simply dummy text of the printing and typesetting industry.\
-            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\
-            It has survived not only five centuries',
+            body: 'hello Ipsum hello is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries'
         },
     ]
 
     // Inserts seed entries
-    await knex('articles').insert(articles);
+    await knex('articles').insert(articles.map(article => {
+        return {
+            ...article,
+            tsvector_body: knex.raw('to_tsvector(?)', [article.body])
+        }
+    }));
 };
