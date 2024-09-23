eb6a146937795fc439c6fdb4b9b3038b8974c0c0
yoni2356 <yoni2356@gmail.com>
2024-09-23 13:39:25 +0300

    handle article errors

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { InjectConnection } from 'nest-knexjs';
 import { Knex } from 'knex';
@@ -10,51 +10,68 @@ export class ArticleService {
   constructor(@InjectConnection() private readonly knex: Knex) { }
 
   async create(createArticleDto: CreateArticleDto): Promise<Article> {
-    const { body, author_id, title } = createArticleDto;
+    try {
+      const { body, author_id, title } = createArticleDto;
+      const [createdArticle] = await this.knex(DATABASE_NAMES.ARTICLES)
+        .insert({
+          author_id,
+          title,
+          body,
+          tsvector_body: this.knex.raw('to_tsvector(?)', [body])
+        })
+        .returning('*');
 
-    const [createdArticle] = await this.knex('articles')
-      .insert({
-        author_id,
-        title,
-        body,
-        tsvector_body: this.knex.raw('to_tsvector(?)', [body])
-      })
-      .returning('*');
-
-    return createdArticle;
+      return createdArticle;
+    }
+    catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException('Article creation failed');
+    }
   }
 
   async findOne(id: number): Promise<Article> {
-    const article = await this.knex(DATABASE_NAMES.ARTICLES).where({ id }).first();
+    try {
+      const article = await this.knex(DATABASE_NAMES.ARTICLES).where({ id }).first();
 
-    if (!article) {
-      throw new NotFoundException();
+      if (!article) {
+        throw new NotFoundException('Article not found');
+      }
+
+      return article;
     }
-
-    return article;
+    catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException('Article find failed')
+    }
   }
 
   async findByWords(words: string[]): Promise<{
     [word: string]: { article_id: number, offsets: number[] }
   }[]> {
-    const query = words.join(' | ');
-    const articles: Article[] = await this.knex('articles')
-      .whereRaw('tsvector_body @@ plainto_tsquery(?)', [query]);
+    try {
+      const query = words.join(' | ');
+      const articles: Article[] = await this.knex(DATABASE_NAMES.ARTICLES)
+        .whereRaw('tsvector_body @@ plainto_tsquery(?)', [query]);
 
-    return articles.map((article) => {
-      const result = {};
-      words.forEach((word) => {
-        // Case-insensitive exact word match
-        const regex = new RegExp(`\\b${word}\\b`, 'gi');
-        const matches = Array.from(article.body.matchAll(regex));
+      return articles.map((article) => {
+        const result = {};
+        words.forEach((word) => {
+          // Case-insensitive exact word match
+          const regex = new RegExp(`\\b${word}\\b`, 'gi');
+          const matches = Array.from(article.body.matchAll(regex));
 
-        const offsets = matches.map(match => match.index);
+          const offsets = matches.map(match => match.index);
 
-        if (offsets.length) {
-          result[word] = { article_id: article.id, offsets };
-        }
+          if (offsets.length) {
+            result[word] = { article_id: article.id, offsets };
+          }
+        });
+        return result;
       });
-      return result;
-    });
+    }
+    catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException('Article find by words failed')
+    }
   }
 }
